4a25aa90d4b068f4c7b7651721e96dd6c21284a4
Sergey Lukin <contact@sergeylukin.com>
2020-07-15 02:31:23 +0300

    Animate logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,20 +1,44 @@
 import React from "react";
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/core'
+
+const spin = keyframes({
+  '0%': {
+    strokeDasharray: '4000',
+    strokeDashoffset: '4000',
+  },
+  '45%': {
+    strokeDasharray: '4000',
+    strokeDashoffset: '4000',
+  },
+  '100%': {
+    strokeDasharray: '4000',
+    strokeDashoffset: '0',
+  }
+})
+
+const StyledLogo = styled.svg`
+  fill: transparent;
+  .myPath {
+    animation: ${spin} 3s ease-in;
+  }
+`
+
 
 function Logo() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
+    <StyledLogo
       width="100%"
-      fill="none"
       viewBox="0 0 904 744"
     >
       <path fill="#D0021B" d="M0 0H904V744H0z"></path>
       <path
         stroke="#fff"
+        className="myPath"
         strokeWidth="18"
         d="M372.512 643.571H17.958S428.341 33.134 442.854 15.008c4.988-6.231 444.865 717.59 444.865 717.59H.607M229.914 484H526M439.711 171.773L731.343 647.81"
       ></path>
-    </svg>
+    </StyledLogo>
   );
 }
 
